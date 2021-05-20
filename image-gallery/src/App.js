6a67c269c73a1fd929527987e7efd99b8469f1f9
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-20 14:48:22 -0700

    working gallery example

---
+++ image-gallery/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Gallery from './Gallery'
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Image Gallery</h1>
+     <Gallery />
+    </div>
+  );
+}
+
+export default App;
